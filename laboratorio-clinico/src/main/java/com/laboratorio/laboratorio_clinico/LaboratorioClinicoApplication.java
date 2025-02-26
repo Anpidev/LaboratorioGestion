d46594ea6ef1a7aaf4d7cfc249ef0e030e05d038
Anpidev <angelaruizdev@gmail.com>
2025-02-26 16:35:58 +0100

    Comienzo

---
+++ laboratorio-clinico/src/main/java/com/laboratorio/laboratorio_clinico/LaboratorioClinicoApplication.java
@@ -0,0 +1,13 @@
+package com.laboratorio.laboratorio_clinico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioClinicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratorioClinicoApplication.class, args);
+	}
+
+}
---
+++ laboratorio-clinico/src/test/java/com/laboratorio/laboratorio_clinico/LaboratorioClinicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratorio.laboratorio_clinico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioClinicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
